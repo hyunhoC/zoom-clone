0f3d740f7721306c2478e0408a3297716823ad33
hyunhoC <whgusgh1357@gmail.com>
2023-01-02 22:38:32 +0900

    added simple chat function using vanilla websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,48 @@
-hello;
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nick");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket.addEventListener("open", () => {
+//   console.log("Connected to Server");
+// });
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+// socket.addEventListener("close", () => {
+//   console.log("Disconnected from Server");
+// });
+
+// setTimeout(() => {
+//   socket.send("hello from the browser!");
+// }, 1000);
+
+const makeMessage = (type, payload) => {
+  const msg = {type, payload};
+  return JSON.stringify(msg)
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+};
+
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -10,4 +12,40 @@ app.get("/", (req, res) => res.render("home"));
 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// const handleConnection = (socket) => {
+//   console.log("Connected to Browser");
+//   socket.on("close", () => {
+//     console.log("Disconnected from the Browser")
+//   });
+//   socket.on("message", message => {
+//     console.log(message.toString());
+//   })
+//   socket.send("hello");
+// };
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message.toString());
+    console.log(parsed);
+
+    switch (parsed.type) {
+      case "message":
+        sockets.forEach(s => s.send(`${socket.nickname}: ${parsed.payload.toString()}`));
+        break;
+      case "nickname":
+        console.log('this is called')
+        socket["nickname"] = parsed.payload.toString();
+        break;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
