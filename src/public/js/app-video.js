f8b7e4093512a66540d19f9d7920975d1cd64cfb
hyunhoC <whgusgh1357@gmail.com>
2023-01-05 20:34:47 +0900

    added camera function

--- src/public/js/app-video.js
+++ src/public/js/app-video.js
@@ -1,19 +1,84 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
-async function getMedia() {
+const getCameras = async () => {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true
-    });
-    console.log(myStream);
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter(device => device.kind === 'videoinput');
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach(camera => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    })
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const getMedia = async (deviceId) => {
+  const initialContrains = {
+    audio: true,
+    video: { facingMode: "user"},
+  };
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: {exact: deviceId }},
+  };
+
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? initialContrains : cameraConstrains
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
 getMedia();
+
+const handleMuteClick = (event) => {
+  myStream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+const handleCameraClick = (event) => {
+  myStream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On"
+    cameraOff = true;
+  }
+}
+
+const handleCameraChange = async () => {
+  await getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
